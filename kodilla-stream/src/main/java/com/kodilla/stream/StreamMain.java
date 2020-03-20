0abb88033cd668102758ef137234372e13dad2ed
jacurrr <jacek.rubach@gmail.com>
2020-03-20 18:24:42 +0100

    Moduł 7.2 Teoria

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -19,5 +20,7 @@ public class StreamMain {
             return result;
         });
 
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
