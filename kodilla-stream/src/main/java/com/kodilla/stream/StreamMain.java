0f04a182d2d349fcec77d0d85cfe202541f46330
jacurrr <jacek.rubach@gmail.com>
2020-03-22 17:29:38 +0100

    Moduł 7.3 Zadanie

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,17 +1,23 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.book.Book;
-import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+import java.time.LocalDate;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-        BookDirectory theBookDirectory = new BookDirectory();
-        String theResultStringOfBooks = theBookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() > 2005)
-                .map(Book::toString)
-                .collect(Collectors.joining(",\n","<<",">>"));
+        Forum forum = new Forum();
+        Map<Integer, ForumUser> theResultMapOfUsers = forum.getUserList().stream()
+                .filter(forumUser -> forumUser.getSex() == 'M')
+                .filter(forumUser -> LocalDate.now().getYear() - forumUser.getBirthDate().getYear() >=20)
+                .filter(forumUser -> forumUser.getPostCounter() > 0)
+                .collect(Collectors.toMap(ForumUser::getID, forumUser -> forumUser));
 
-        System.out.println(theResultStringOfBooks);
+        for (Map.Entry<Integer, ForumUser> entry :theResultMapOfUsers.entrySet())
+        {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
