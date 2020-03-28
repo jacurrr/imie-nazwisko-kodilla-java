e3628bb29dcf295d9fca96d93afd45bc5fa8b39b
jacurrr <jacek.rubach@gmail.com>
2020-03-28 20:46:08 +0100

    Moduł 8.3 Zadanie

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,14 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try {
+            secondChallenge.probablyIWillThrowException(1.5, 1.5);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Program still works!");
+        }
+    }
+}
