a1d1287c41d3046b3771503f8bc2d78b511cdc3c
jacurrr <jacek.rubach@gmail.com>
2020-03-28 16:59:42 +0100

    Moduł 8.2 Zadanie

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -2,7 +2,7 @@ package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
 
-public class ExcptionModuleRunner {
+public class ExceptionModuleRunner {
     public static void main(String args[]) {
         FileReader fileReader = new FileReader();
         fileReader.readFile();
