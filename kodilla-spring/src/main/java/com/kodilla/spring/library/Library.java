c4f97a919c52776ecebc66dc17ef702af2c9a7b2
jacurrr <jacek.rubach@gmail.com>
2020-05-16 22:43:44 +0200

    Moduł 14.3 Teoria

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -1,20 +1,12 @@
 package com.kodilla.spring.library;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
 public class Library {
     private  final List<String> books = new ArrayList<>();
-
-    @Autowired
     private  LibraryDbController libraryDbController;
 
-    /*
-    @Autowired
     public Library(final  LibraryDbController libraryDbController){
         this.libraryDbController = libraryDbController;
     }
@@ -22,13 +14,7 @@ public class Library {
     public Library() {
         //do nothing
     }
-    */
-    /*
-    @Autowired
-    public void setLibraryDbController(LibraryDbController libraryDbController){
-        this.libraryDbController = libraryDbController;
-    }
-    */
+
     public  void  saveToDb() {
         libraryDbController.saveData();
     }
