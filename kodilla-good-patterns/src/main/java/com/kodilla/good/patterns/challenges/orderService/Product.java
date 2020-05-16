3ba606fc4f82ec7342de3524eb7907ca64ecf71d
jacurrr <jacek.rubach@gmail.com>
2020-05-16 17:53:53 +0200

    Moduł 14.1 Teoria

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/orderService/Product.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/orderService/Product.java
@@ -3,7 +3,7 @@ package com.kodilla.good.patterns.challenges.orderService;
 public class Product {
     private String name;
     private double price;
-    Product(String name, double price){
+    public Product(String name, double price){
         this.name = name;
         this.price = price;
     }
